52fdf99ba8c3a024e75354254d39cb735dd1df97
Rohit <rohit.rthd.04@gmail.com>
2026-01-30 23:25:29 +0530

    configed tailwind

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || '';
 if(!API_BASE_URL){
+  console.error('VITE_API_URL is not defined in environment variables');
   throw new Error('VITE_API_URL is not defined in environment variables');
 }
+console.log('<--------------- API Base URL: -------------->', API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
